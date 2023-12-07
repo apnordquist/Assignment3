27f622fe9fd93128c01b8a22d5682d48c55410df
Adam Nordquist <apnordquist@gmail.com>
2023-12-07 14:20:32 -0700

    updating js

--- scripts/booking.js
+++ scripts/booking.js
@@ -3,30 +3,41 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 
-let dayRate = 35
+let dayRate = 35;
+let numDays = 0;
 
+/********* calculate *********/
+// when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 
+let calculatedCost = document.getElementById("calculated-cost");
+
+function recalculate(){
+    var totalCost = dayRate * numDays;
+    calculatedCost.innerHTML = totalCost;
+}
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 let mondayButton = document.getElementById("monday");
-mondayButton.addEventListener("click", highlight(mondayButton));
+mondayButton.addEventListener("click", highlight);
 
 let tuesdayButton = document.getElementById("tuesday");
-tuesdayButton.addEventListener("click", highlight(tuesdayButton));
+tuesdayButton.addEventListener("click", highlight);
 
 let wednesdayButton = document.getElementById("wednesday");
-wednesdayButton.addEventListener("click", highlight(wednesdayButton));
+wednesdayButton.addEventListener("click", highlight);
 
 let thursdayButton = document.getElementById("thursday");
-thursdayButton.addEventListener("click", highlight(thursdayButton));
+thursdayButton.addEventListener("click", highlight);
 
 let fridayButton = document.getElementById("friday");
-fridayButton.addEventListener("click", highlight(fridayButton));
+fridayButton.addEventListener("click", highlight);
 // not sure why function is not working correctly
-function highlight(dayButton) {
-    dayButton.classList.add("clicked");
+function highlight() {
+    this.classList.contains("clicked") ? numDays += 0 : numDays += 1;
+    this.classList.add("clicked");
+    recalculate();
 }
 
 /********* clear days *********/
@@ -40,6 +51,8 @@ function clearDays() {
     wednesdayButton.classList.remove("clicked");
     thursdayButton.classList.remove("clicked");
     fridayButton.classList.remove("clicked");
+    numDays = 0;
+    recalculate();
 }
 
 /********* change rate *********/
@@ -47,23 +60,22 @@ function clearDays() {
 let halfDay = document.getElementById("half");
 let fullDay = document.getElementById("full");
 
-halfDay.addEventListener("click",changeRate)
+halfDay.addEventListener("click", halfRate);
 
-function changeRate () {
-    fullDay.classList.contains("clicked") ? fullDay.classList.remove("clicked") : fullDay.classList.add("clicked");
-    halfDay.classList.contains("clicked") ? halfDay.classList.remove("clicked") : halfDay.classList.add("clicked");
-    halfDay.classList.contains("clicked") ? dayRate = 20 : dayRate = 35
+function halfRate () {
+    fullDay.classList.remove("clicked");
+    halfDay.classList.add("clicked");
+    dayRate = 20;
+    recalculate();
 }
-
-
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
+fullDay.addEventListener("click", fullRate);
 
-
-
-
-/********* calculate *********/
-// when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
-
-
+function fullRate () {
+    fullDay.classList.add("clicked");
+    halfDay.classList.remove("clicked");
+    dayRate = 35;
+    recalculate();
+}
--- scripts/contact.js
+++ scripts/contact.js
@@ -1,6 +1,6 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 
-let contactPage = document.getElementById("contact-page")
+let contactPage = document.getElementById("contact-page");
 
 let submitButton = document.getElementById("submit-button");
 
